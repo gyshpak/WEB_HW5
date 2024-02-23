3f6d5a49997c6216bd7ae9642c720c0c66ca2473
Gennadiy Shpak <gena_shpak@ukr.net>
2024-02-23 19:41:21 +0200

    comm_23.02.24_19.41

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import asyncio
+import aiohttp
 import logging
 import websockets
 import names
+from datetime import date, datetime, timedelta
 from websockets import WebSocketServerProtocol
 from websockets.exceptions import ConnectionClosedOK
 
@@ -35,7 +37,36 @@ class Server:
     
     async def distribute(self, ws: WebSocketServerProtocol):
         async for message in ws:
-            await self.sent_to_clients(f'{ws.name}: {message}')
+            ansver_mess = await check_mess(message)
+            await self.sent_to_clients(f'{ws.name}: {ansver_mess}')
+
+async def check_mess(message: str) -> str:
+    # logging.info(f'query1 = {message}')
+    if message.split(" ")[0] == 'exchange':
+        # logging.info(f'query2 = {message}')
+        if message.split(" ")[1].isdigit():
+            # logging.info(f'query3 = {message}')
+            quant_ = message.split(" ")[1]
+            logging.info(f'quant_ = {quant_}')
+            date_ =  date.today()
+            logging.info(f'date_ = {date_}')
+            for i in range(quant_):
+                logging.info(f'iteration = {i}')
+                current_date = (date_ - timedelta(days=i)).strftime('%d.%m.%Y')
+                logging.info(f'query6 = {current_date}')
+                result = await request_to_banc(f'https://api.privatbank.ua/p24api/exchange_rates?json&date={current_date}')
+                logging.info(f'query8 = {result}')
+                
+    else:
+        result = message
+    return result
+
+async def request_to_banc(message):
+    logging.info(f'query7 = {message}')
+    async with aiohttp.ClientSession() as session:            
+        async with session.get(message) as response:
+            result = await response.json()
+            return result
     
 async def main():
     server = Server()
